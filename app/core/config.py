a1872367b4bb97b71387565556459594a4c64895
laamiri-kaoutar <kaoutarlaamiri355@gmail.com>
2026-01-05 12:27:18 +0100

    config: setup pydantic settings to load env variables

---
+++ app/core/config.py
@@ -0,0 +1,18 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str = "YouLogiX"
+    API_V1_STR: str = "/api/v1"
+    
+    # Database Settings (Loaded from .env)
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_SERVER: str
+    POSTGRES_PORT: int
+    POSTGRES_DB: str
+    DATABASE_URL: str
+
+    # This tells Pydantic to read the .env file
+    model_config = SettingsConfigDict(env_file=".env", case_sensitive=True)
+
+settings = Settings()
